25ea95856e8f62199ee33e99798a455a6caae512
Lucas Villarrubia <villar.lucase@gmail.com>
2025-11-16 20:15:06 -0300

    [fix] Update en el client de la API para conectar con ngrok

--- frontend/src/lib/apiClient.ts
+++ frontend/src/lib/apiClient.ts
@@ -22,6 +22,7 @@ apiClient.interceptors.request.use(
         if (token) {
             config.headers.Authorization = `Bearer ${token}`;
         }
+        config.headers['ngrok-skip-browser-warning'] = 'true';
         return config;
     },
     (error) => {
